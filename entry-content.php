c7bbc04ada529acf41a1de22db657ee42c47ad61
Swapnil Tawar <swapnil.tawar@hbwsl.com>
2016-09-29 14:59:50 +0530

    New feature and compatibility changes added

--- entry-content.php
+++ entry-content.php
@@ -1,5 +1,23 @@
-<section class="entry-content">
-<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-<?php the_content(); ?>
-<div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
+<?php if( is_home() ) :// blog post pages ?>
+  		<?php if( get_option( 'fad_post_excerpts') != 0 ): ?>
+  			<section class="entry-content">
+			<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+        			<?php the_excerpt(); ?>
+				<div class="entry-links"><?php wp_link_pages(); ?></div>
+      			</section>
+    <?php else: ?>
+		    	<section class="entry-content">
+				<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+				<?php the_content( __( 'Continue reading', 'fad' ) . '<span class="meta-nav">&rarr;</span>' ); ?>
+				<div class="entry-links"><?php wp_link_pages(); ?></div>
+			</section>
+    <?php endif; ?>
+	
+	<?php else: ?>
+		<section class="entry-content">
+			<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+			<?php the_content(); ?>
+			<div class="entry-links"><?php wp_link_pages(); ?></div>
+		</section>
+		
+	<?php endif; ?>
